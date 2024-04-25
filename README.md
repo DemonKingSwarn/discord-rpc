2c1d09da37c3b593cf1d61d7191d4d79519172dd
DemonKingSwarn <rockingswarn@gmail.com>
2024-04-25 23:22:48 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Discord RPC
+
+A simple script to create Discord RPC without using any external library like pypresence.
+
+**NOTE**: This only works on Linux and MacOS as of now
+
+## Usage
+
+```sh
+python rpc.py <discord client id> <path to payload.json>
+```
---
+++ rpc-linux.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+import struct
+import json
+import socket
+import sys
+import time
+
+def encode_data(data, opcode):
+    length = len(data)
+    payload = struct.pack('<L', length)
+    return payload + struct.pack('<L', opcode) + data.encode('utf-8')
+
+if len(sys.argv) < 3:
+    print("Pass client Id and Payload please!!")
+    sys.exit(1)
+
+presence_client_id = sys.argv[1]
+discord_ipc = os.getenv('XDG_RUNTIME_DIR') + "/discord-ipc-0"
+
+try:
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+        s.connect(discord_ipc)
+        
+        # Handshake
+        handshake_data = json.dumps({'v': 1, 'client_id': presence_client_id})
+        s.sendall(encode_data(handshake_data, 0))
+
+        # Wait (max 4 seconds) to receive the output
+        time.sleep(4)
+
+        # Set activity
+        payload_data = sys.argv[2]
+        s.sendall(encode_data(payload_data, 1))
+except FileNotFoundError:
+    print("Discord IPC not found. Make sure Discord is running.")
+except Exception as e:
+    print("An error occurred:", e)
